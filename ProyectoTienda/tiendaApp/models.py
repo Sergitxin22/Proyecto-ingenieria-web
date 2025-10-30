1e482e9dd33cc7c4f00de7a0fc1921bf484c9ab7
aroa-im <aroa.i.m@opendeusto.es>
2025-10-30 08:48:59 +0100

    modelo acabado

--- ProyectoTienda/tiendaApp/models.py
+++ ProyectoTienda/tiendaApp/models.py
@@ -15,3 +15,25 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Prenda(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+    talla = models.CharField(max_length=100)
+    precio = models.CharField(max_length=100)
+    foto = models.URLField(max_length=200, blank=True, null=True)
+
+    class Meta: #Para visualizat el nombre en singular y plural del modelo en ADMIN
+        verbose_name = "Prenda"
+        verbose_name_plural = "Prendas"
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    precio = models.CharField(max_length=100, blank=True)
+    cliente = models.ForeignKey(Cliente, on_delete= models.CASCADE, related_name='usuarioPedido')
+    prendas = models.ManyToManyField(Prenda, related_name="prendasPedido")
+    class Meta: #Para visualizat el nombre en singular y plural del modelo en ADMIN
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
