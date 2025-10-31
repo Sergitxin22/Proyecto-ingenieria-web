bf2535d0188d24f504919faed75b26700d9d10b7
Sergitxin22 <sergio.morales@opendeusto.es>
2025-10-31 21:04:35 +0100

    Agregar vista y plantilla para listar prendas

--- ProyectoTienda/tiendaApp/urls.py
+++ ProyectoTienda/tiendaApp/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns=[
     path('', views.index, name='listaTienda'),
+    path('prendas/', views.lista_prendas, name='lista_prendas'),
 ]
--- ProyectoTienda/tiendaApp/views.py
+++ ProyectoTienda/tiendaApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Prenda, Pedido
 
 def index(request):
     return HttpResponse('primera vista')
 # Create your views here.
+
+def lista_prendas(request):
+    prendas = Prenda.objects.all()
+    context = {'prendas': prendas}
+    return render(request, 'prendas/lista_prendas.html', context)
