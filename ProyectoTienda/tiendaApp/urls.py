07f98313430edc2638b2efc7ffef5247962da020
SamiraArmoa <s.armoa@opendeusto.es>
2025-11-04 13:46:54 +0100

    clientes vista

--- ProyectoTienda/tiendaApp/urls.py
+++ ProyectoTienda/tiendaApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('', views.index, name='listaTienda'),
     path('prendas/', views.lista_prendas, name='lista_prendas'),
     path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
+    path('clientes/', views.lista_clientes, name='lista_clientes'),
 ]
--- ProyectoTienda/tiendaApp/views.py
+++ ProyectoTienda/tiendaApp/views.py
@@ -15,3 +15,8 @@ def lista_pedidos(request):
     pedidos = Pedido.objects.all()
     context = {"pedidos": pedidos}
     return render(request, 'pedidos/lista_pedidos.html', context)
+
+def lista_clientes(request):
+    clientes = Cliente.objects.all()
+    context = {"clientes": clientes}
+    return render(request, 'clientes/lista_clientes.html', context)
