834ebeaaa25012901ea511db3f92fa2d5799df35
Oier Artabe <oier.artabe@opendeusto.es>
2025-11-04 13:38:22 +0100

    urlsPedidos

--- ProyectoTienda/tiendaApp/urls.py
+++ ProyectoTienda/tiendaApp/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns=[
     path('', views.index, name='listaTienda'),
     path('prendas/', views.lista_prendas, name='lista_prendas'),
+    path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
 ]
