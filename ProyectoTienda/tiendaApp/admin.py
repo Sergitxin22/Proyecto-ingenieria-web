32a42d46b805dfac5bf41cb0d3bc18603490702d
SamiraArmoa <s.armoa@opendeusto.es>
2025-11-24 09:19:02 +0100

    admin categoria

--- ProyectoTienda/tiendaApp/admin.py
+++ ProyectoTienda/tiendaApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Cliente, Prenda, Pedido
+from .models import Cliente, Prenda, Pedido,Categoria
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Prenda)
 admin.site.register(Pedido)
+admin.site.register(Categoria)
