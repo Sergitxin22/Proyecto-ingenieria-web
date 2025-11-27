f51682e8f1b998a2755f9074e3e988d81b4b71bb
Sergitxin22 <sergio.morales@opendeusto.es>
2025-11-27 17:08:21 +0100

    Registrar modelo FotoPrenda

--- ProyectoTienda/tiendaApp/admin.py
+++ ProyectoTienda/tiendaApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Cliente, Prenda, Pedido,Categoria
+from .models import Cliente, Prenda, Pedido, Categoria, FotoPrenda
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Prenda)
 admin.site.register(Pedido)
 admin.site.register(Categoria)
+admin.site.register(FotoPrenda)
--- ProyectoTienda/tiendaApp/models.py
+++ ProyectoTienda/tiendaApp/models.py
@@ -9,7 +9,7 @@ class Cliente(models.Model):
     foto = models.URLField(max_length=200, blank=True, null=True)
     #siempre que añada nuevos campos y ya tengo info en BD, ponemos blank true y null true
 
-    class Meta: #Para visualizat el nombre en singular y plural del modelo en ADMIN
+    class Meta: #Para visualizar el nombre en singular y plural del modelo en ADMIN
         verbose_name = "Cliente"
         verbose_name_plural = "Clientes"
 
@@ -19,7 +19,7 @@ class Cliente(models.Model):
 class Categoria(models.Model):
     categoria = models.CharField(max_length=100)
     
-    class Meta: #Para visualizat el nombre en singular y plural del modelo en ADMIN
+    class Meta: #Para visualizar el nombre en singular y plural del modelo en ADMIN
         verbose_name = "Categoria"
         verbose_name_plural = "Categorias"
 
@@ -31,11 +31,9 @@ class Prenda(models.Model):
     descripcion = models.CharField(max_length=100)
     talla = models.CharField(max_length=100)
     precio = models.CharField(max_length=100)
-    foto = models.URLField(max_length=200, blank=True, null=True)
     categorias = models.ManyToManyField(Categoria, related_name="categoriasPrenda")
 
-
-    class Meta: #Para visualizat el nombre en singular y plural del modelo en ADMIN
+    class Meta:
         verbose_name = "Prenda"
         verbose_name_plural = "Prendas"
 
@@ -48,6 +46,13 @@ class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete= models.CASCADE, related_name='usuarioPedido')
     prendas = models.ManyToManyField(Prenda, related_name="prendasPedido")
 
-    class Meta: #Para visualizat el nombre en singular y plural del modelo en ADMIN
+    class Meta: #Para visualizar el nombre en singular y plural del modelo en ADMIN
         verbose_name = "Pedido"
         verbose_name_plural = "Pedidos"
+
+class FotoPrenda(models.Model):
+    prenda = models.ForeignKey(Prenda, on_delete=models.CASCADE, related_name="fotos")
+    url = models.URLField(max_length=300)
+
+    def __str__(self):
+        return f"Foto de {self.prenda.nombre}"
