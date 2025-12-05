4d896d4a26263f8f7587371fc238f1e4ad7fa193
aroa-im <aroa.i.m@opendeusto.es>
2025-12-05 18:32:03 +0100

    cambiar urls.py

--- ProyectoTienda/ProyectoTienda/urls.py
+++ ProyectoTienda/ProyectoTienda/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tiendaApp/', include('tiendaApp.urls')),
+    path('', include('tiendaApp.urls')),
 ]
